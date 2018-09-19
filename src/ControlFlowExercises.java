5a27e5d7771e426058c4cbcb9000a1b10598c420
patrickrfisher <patrick.ryan.fisher@gmail.com>
2018-09-19 10:21:59 -0500

    java 8/19

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,6 +2,46 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+//        int base = 5
+//        System.out.println("Continue? [y/n]");
+//        String userInput = sc.next();
+//        boolean confirmation = userInput.equals("y");
+//-----------------------------------------------------------
+//        int caseSwitch = 1;
+//
+//        switch (caseSwitch) {
+//            case 1:
+//                System.out.println("Case 1");
+//            case 2:
+//                System.out.println("Case 2");
+//                break;
+//            default:
+//                System.out.println("Default case");
+//                break;
+//        }
+//    }
 
-    }
-}
+    //---------------------------------------------------------------
+
+//        int i =0;
+//        while (i<29){
+//            System.out.println("i is"+i);
+//            i++;
+//        }
+//
+//--------------------------------------------
+        int i=5;
+        while(i<=15){
+            System.out.println("i is"+i);
+            i++;
+        }
+
+
+
+    }}
+
+
+
+
+
+
